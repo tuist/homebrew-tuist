31def3411958d543ab1b29cbc45424c8029ca954
vanyauhalin <vanyauhalin@gmail.com>
2023-04-14 23:55:28 +0400

    Bump to v3.18.0

--- Formula/tuist.rb
+++ Formula/tuist.rb
@@ -1,10 +1,10 @@
 class Tuist < Formula
   desc "Create, maintain, and interact with Xcode projects at scale"
-  homepage "https://tuist.io/"
-  url "https://github.com/tuist/tuist/releases/download/3.17.0/tuist.zip"
-  sha256 "43c551f9c9fd729b989218f9e2ebc0bebd44af1f888119d398aac829d0f7231c"
+  homepage "https://tuist.io"
+  url "https://github.com/tuist/tuist/releases/download/3.18.0/tuist.zip"
+  sha256 "afec90f79ff620821d56c3bcc361c25b7fb9bb876cceb77c0c38acc4921ff959"
   license "MIT"
-  head "https://github.com/tuist/tuist/", branch: "main"
+  head "https://github.com/tuist/tuist", branch: "main"
 
   depends_on macos: :monterey
 
@@ -14,7 +14,7 @@ class Tuist < Formula
 
   test do
     Open3.popen3("#{bin}/tuist", "version") do |_, stdout, _|
-      assert_match "3.17.0", stdout.read
+      assert_match "3.18.0", stdout.read
     end
   end
 end
