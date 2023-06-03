d26e9592578a6410c0cfd4a34b0984f8e55cee76
vanyauhalin <vanyauhalin@gmail.com>
2023-06-03 23:15:01 +0400

    Add build stage

--- Formula/tuist.rb
+++ Formula/tuist.rb
@@ -1,14 +1,36 @@
 class Tuist < Formula
   desc "Create, maintain, and interact with Xcode projects at scale"
   homepage "https://tuist.io"
-  url "https://github.com/tuist/tuist/releases/download/3.18.0/tuist.zip"
-  sha256 "afec90f79ff620821d56c3bcc361c25b7fb9bb876cceb77c0c38acc4921ff959"
+  url "https://github.com/tuist/tuist/archive/refs/tags/3.18.0.tar.gz"
+  sha256 "6d8a108560de2fe168cdeca3d63441b09de2faaa83efc433d37c00c076c16ea0"
   license "MIT"
-  head "https://github.com/tuist/tuist", branch: "main"
+  head "https://github.com/tuist/tuist.git", branch: "main"
 
+  # https://github.com/tuist/tuist/blob/3.18.0/.xcode-version#L1
+  depends_on xcode: ["13.4.1", :build]
+
+  # https://github.com/tuist/tuist/blob/3.18.0/Package.swift#L14
   depends_on macos: :monterey
 
   def install
-    bin.install Dir["*"]
+    # https://github.com/tuist/tuist/blob/3.18.0/.github/workflows/tuist.yml#L81
+    system "swift", "build",
+      "--configuration", "release",
+      "--product", "tuist",
+      "--disable-sandbox"
+
+    # https://github.com/tuist/tuist/blob/3.18.0/.github/workflows/tuist.yml#L85
+    system "swift", "build",
+      "--configuration", "release",
+      "--product", "ProjectDescription",
+      "--disable-sandbox"
+
+    # https://github.com/tuist/tuist/blob/3.18.0/Sources/TuistEnvKit/Installer/BuildCopier.swift#L13
+    bin.install ".build/release/tuist"
+    bin.install "Templates"
+    bin.install "projects/tuist/vendor"
+    bin.install ".build/release/ProjectDescription.swiftmodule"
+    bin.install ".build/release/ProjectDescription.swiftdoc"
+    bin.install ".build/release/libProjectDescription.dylib"
   end
 end
