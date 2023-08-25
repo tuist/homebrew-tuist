56da0ec0f3d174dbc1c59dc867d75d547744c82b
vanyauhalin <vanyauhalin@gmail.com>
2023-08-25 08:53:34 +0400

    add v3.23.1

---
+++ Formula/tuist@3.23.1.rb
@@ -0,0 +1,36 @@
+class TuistAT3231 < Formula
+  desc "Create, maintain, and interact with Xcode projects at scale"
+  homepage "https://tuist.io"
+  url "https://github.com/tuist/tuist/archive/refs/tags/3.23.1.tar.gz"
+  sha256 "37af2136738f7dbd09a61cf5556054810be6fc91583d9238088899ffa0de135f"
+  license "MIT"
+  head "https://github.com/tuist/tuist.git", branch: "main"
+
+  # https://github.com/tuist/tuist/blob/3.23.1/.xcode-version#L1
+  depends_on xcode: ["14.3", :build]
+
+  # https://github.com/tuist/tuist/blob/3.23.1/Package.swift#L14
+  depends_on macos: :monterey
+
+  def install
+    # https://github.com/tuist/tuist/blob/3.23.1/.github/workflows/tuist.yml#L81
+    system "swift", "build",
+      "--configuration", "release",
+      "--product", "tuist",
+      "--disable-sandbox"
+
+    # https://github.com/tuist/tuist/blob/3.23.1/.github/workflows/tuist.yml#L85
+    system "swift", "build",
+      "--configuration", "release",
+      "--product", "ProjectDescription",
+      "--disable-sandbox"
+
+    # https://github.com/tuist/tuist/blob/3.23.1/Sources/TuistEnvKit/Installer/BuildCopier.swift#L13
+    bin.install ".build/release/tuist"
+    bin.install "Templates"
+    bin.install "projects/tuist/vendor"
+    bin.install ".build/release/ProjectDescription.swiftmodule"
+    bin.install ".build/release/ProjectDescription.swiftdoc"
+    bin.install ".build/release/libProjectDescription.dylib"
+  end
+end
