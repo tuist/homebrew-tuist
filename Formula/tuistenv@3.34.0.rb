57854f6a352e5cdcdd1429ad95b45d5cb0f2d34e
Daniel Bachar <danielbac@zillowgroup.com>
2023-11-30 08:48:47 -0700

    Release 3.34.0

---
+++ Formula/tuistenv@3.34.0.rb
@@ -0,0 +1,25 @@
+class TuistenvAT3340 < Formula
+    desc "Managing Tuist versions"
+    homepage "https://tuist.io"
+    url "https://github.com/tuist/tuist/archive/refs/tags/3.34.0.tar.gz"
+    sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+    license "MIT"
+    head "https://github.com/tuist/tuist.git", branch: "main"
+  
+    # https://github.com/tuist/tuist/blob/3.33.4/.xcode-version#L1
+    depends_on xcode: ["14.3", :build]
+  
+    # https://github.com/tuist/tuist/blob/3.33.4/Package.swift#L14
+    depends_on macos: :monterey
+  
+    def install
+      # https://github.com/tuist/tuist/blob/3.33.4/.github/workflows/tuist.yml#L83
+      system "swift", "build",
+        "--configuration", "release",
+        "--product", "tuistenv",
+        "--disable-sandbox"
+  
+      bin.install ".build/release/tuistenv"
+    end
+  end
+  
