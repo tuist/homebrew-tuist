1414eccb077c64b76e383bd1e0380867dbff8313
Daniel Bachar <danielbac@zillowgroup.com>
2023-11-30 08:54:16 -0700

    small cosmetics

--- Formula/tuist@3.34.0.rb
+++ Formula/tuist@3.34.0.rb
@@ -6,26 +6,26 @@ class TuistAT3340 < Formula
   license "MIT"
   head "https://github.com/tuist/tuist.git", branch: "main"
 
-  # https://github.com/tuist/tuist/blob/3.33.4/.xcode-version#L1
+  # https://github.com/tuist/tuist/blob/3.34.0/.xcode-version#L1
   depends_on xcode: ["14.3", :build]
 
-  # https://github.com/tuist/tuist/blob/3.33.4/Package.swift#L14
+  # https://github.com/tuist/tuist/blob/3.34.0/Package.swift#L14
   depends_on macos: :monterey
 
   def install
-    # https://github.com/tuist/tuist/blob/3.33.4/.github/workflows/tuist.yml#L81
+    # https://github.com/tuist/tuist/blob/3.34.0/.github/workflows/tuist.yml#L81
     system "swift", "build",
       "--configuration", "release",
       "--product", "tuist",
       "--disable-sandbox"
 
-    # https://github.com/tuist/tuist/blob/3.33.4/.github/workflows/tuist.yml#L85
+    # https://github.com/tuist/tuist/blob/3.34.0/.github/workflows/tuist.yml#L85
     system "swift", "build",
       "--configuration", "release",
       "--product", "ProjectDescription",
       "--disable-sandbox"
 
-    # https://github.com/tuist/tuist/blob/3.33.4/Sources/TuistEnvKit/Installer/BuildCopier.swift#L13
+    # https://github.com/tuist/tuist/blob/3.34.0/Sources/TuistEnvKit/Installer/BuildCopier.swift#L13
     bin.install ".build/release/tuist"
     bin.install "Templates"
     bin.install "projects/tuist/vendor"
--- Formula/tuistenv@3.34.0.rb
+++ Formula/tuistenv@3.34.0.rb
@@ -6,14 +6,14 @@ class TuistenvAT3340 < Formula
     license "MIT"
     head "https://github.com/tuist/tuist.git", branch: "main"
   
-    # https://github.com/tuist/tuist/blob/3.33.4/.xcode-version#L1
+    # https://github.com/tuist/tuist/blob/3.34.0/.xcode-version#L1
     depends_on xcode: ["14.3", :build]
   
-    # https://github.com/tuist/tuist/blob/3.33.4/Package.swift#L14
+    # https://github.com/tuist/tuist/blob/3.34.0/Package.swift#L14
     depends_on macos: :monterey
   
     def install
-      # https://github.com/tuist/tuist/blob/3.33.4/.github/workflows/tuist.yml#L83
+      # https://github.com/tuist/tuist/blob/3.34.0/.github/workflows/tuist.yml#L83
       system "swift", "build",
         "--configuration", "release",
         "--product", "tuistenv",
