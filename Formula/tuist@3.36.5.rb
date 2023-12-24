2f55d23286442ee5a5621558fad6a5f856930e8a
Marek Fořt <marekfort@me.com>
2023-12-24 17:49:34 +0100

    Update Tuist formula to version 3.36.5

---
+++ Formula/tuist@3.36.5.rb
@@ -0,0 +1,23 @@
+class TuistAT3365 < Formula
+  desc "Create, maintain, and interact with Xcode projects at scale"
+  homepage "https://tuist.io"
+  url "https://github.com/tuist/tuist/releases/download/3.36.5/tuist.zip"
+  sha256 "5d00d6423faaae8100454adc157e531f6d9e2910969dd60a5681509952eec777"
+  license "MIT"
+  head "https://github.com/tuist/tuist.git", branch: "main"
+
+  depends_on macos: :monterey
+
+  def install
+    bin.install "tuist"
+    lib.install "ProjectDescription.framework"
+    lib.install "ProjectDescription.framework.dSYM"
+    lib.install "ProjectAutomation.xcframework"
+    lib.install "ProjectAutomation.xcframework.dSYM"
+    share.install "Templates"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("${bin}/tuist version")
+  end
+end
