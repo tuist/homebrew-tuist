97f505af8cea69769d9f8a97a3228c332e08d5f0
Marek Fořt <marekfort@me.com>
2024-02-12 18:31:08 +0100

    Update Tuist formula to version 4.1.0

---
+++ Formula/tuist@4.1.0.rb
@@ -0,0 +1,23 @@
+class TuistAT410 < Formula
+  desc "Create, maintain, and interact with Xcode projects at scale"
+  homepage "https://tuist.io"
+  url "https://github.com/tuist/tuist/releases/download/4.1.0/tuist.zip"
+  sha256 "78ba7dde521a924f18b9ece58c3fccfedc41657b8d72c1af767bc06ffa9b6236"
+  license "MIT"
+  head "https://github.com/tuist/tuist.git", branch: "main"
+
+  depends_on macos: :monterey
+
+  def install
+    bin.install "tuist"
+    lib.install "ProjectDescription.framework"
+    lib.install "ProjectDescription.framework.dSYM"
+    lib.install "ProjectAutomation.xcframework"
+    lib.install "ProjectAutomation.xcframework.dSYM"
+    share.install "Templates"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("${bin}/tuist version")
+  end
+end
