784ee80e554a8ae1ecfc5333dcd9361fad5d342a
Pedro <pedro@pepicrft.me>
2024-10-18 12:10:59 +0200

    Include Tuist 4.30

---
+++ Formula/tuist@4.30.0.rb
@@ -0,0 +1,21 @@
+class TuistAT4300 < Formula
+  desc "Create, maintain, and interact with Xcode projects at scale"
+  homepage "https://tuist.io"
+  url "https://github.com/tuist/tuist/releases/download/4.30.0/tuist.zip"
+  sha256 "b761881f5f7b6188ac8de62c9887775eec148fb6c8e3b982db281d353a300542"
+  license "MIT"
+  head "https://github.com/tuist/tuist.git", branch: "main"
+
+  depends_on macos: :monterey
+
+  def install
+    bin.install "tuist"
+    lib.install "ProjectDescription.framework"
+    lib.install "ProjectDescription.framework.dSYM"
+    share.install "Templates"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("${bin}/tuist version")
+  end
+end
