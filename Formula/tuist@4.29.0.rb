f37cb6ef689d763904564e4d94c129c1609d3c40
Pedro <pedro@pepicrft.me>
2024-10-21 15:52:41 +0200

    Add missing formulas

---
+++ Formula/tuist@4.29.0.rb
@@ -0,0 +1,21 @@
+class TuistAT4290 < Formula
+  desc "Create, maintain, and interact with Xcode projects at scale"
+  homepage "https://tuist.io"
+  url "https://github.com/tuist/tuist/releases/download/4.29.0/tuist.zip"
+  sha256 "c44c075fd52c341a48ea5995a0ea56b529b2db38bf9b4452c73b3c90e3cba369"
+  license "MIT"
+  head "https://github.com/tuist/tuist.git", branch: "main"
+
+  depends_on macos: :monterey
+
+  def install
+    bin.install "tuist"
+    lib.install "ProjectDescription.framework"
+    lib.install "ProjectDescription.framework.dSYM"
+    share.install "Templates"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("${bin}/tuist version")
+  end
+end
