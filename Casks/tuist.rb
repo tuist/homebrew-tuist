a5f95f46b032713b027f3cfae79d4a2516e03c90
fortmarek <marekfort@me.com>
2024-11-28 13:09:26 +0100

    Update latest tuist cask

--- Casks/tuist.rb
+++ Casks/tuist.rb
@@ -1,8 +1,8 @@
-cask "tuist@0.7.0" do
-    version "0.7.0"
+cask "tuist@0.8.1" do
+    version "0.8.1"
     sha256 "4e121a0fa1803a9837cbfce740bedbd03531815df69ebd69dc92d555ab6b22f2"
 
-    url "https://github.com/tuist/tuist/releases/download/app@0.7.0/Tuist.dmg"
+    url "https://github.com/tuist/tuist/releases/download/app@0.8.1/Tuist.dmg"
     name "Tuist"
     desc "Tuist macOS app"
     homepage "https://github.com/tuist/tuist"
