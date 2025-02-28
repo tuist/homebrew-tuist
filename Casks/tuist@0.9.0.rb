7ce2c66b959ad979643dcc81883c740804ae14cd
fortmarek <marekfort@me.com>
2025-02-28 18:17:00 +0100

    Fix cask sha256

--- Casks/tuist@0.9.0.rb
+++ Casks/tuist@0.9.0.rb
@@ -1,6 +1,6 @@
 cask "tuist@0.9.0" do
     version "0.9.0"
-    sha256 "4e121a0fa1803a9837cbfce740bedbd03531815df69ebd69dc92d555ab6b22f2"
+    sha256 "068bd7cdf3193590aaf560b2e403701b6f6f75f5537df9b67344c0ad0c9c7be9"
 
     url "https://github.com/tuist/tuist/releases/download/app@0.9.0/Tuist.dmg"
     name "Tuist"
