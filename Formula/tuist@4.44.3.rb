714ab0d4ceb92cd571d48b9cadbca87f29ccda45
tuistit <187030322+tuistit@users.noreply.github.com>
2025-03-17 17:50:55 +0000

    Release 4.44.3

---
+++ Formula/tuist@4.44.3.rb
@@ -0,0 +1,21 @@
+class TuistAT4443 < Formula
+  desc "Create, maintain, and interact with Xcode projects at scale"
+  homepage "https://tuist.io"
+  url "https://github.com/tuist/tuist/releases/download/4.44.3/tuist.zip"
+  sha256 "345cd256e56ce02f6f3a90801fa38952522e7159de4097c8b980ee67aaf6ad12"
+  license "MIT"
+  head "https://github.com/tuist/tuist.git", branch: "main"
+
+  depends_on macos: :monterey
+
+  def install
+    bin.install "tuist"
+    lib.install "ProjectDescription.framework"
+    lib.install "ProjectDescription.framework.dSYM"
+    share.install "Templates"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("${bin}/tuist version")
+  end
+end
