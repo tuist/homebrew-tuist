7d1af7ff792fb874e9a5605819fe87b3eb4465a7
tuistit <187030322+tuistit@users.noreply.github.com>
2025-07-23 11:16:42 +0000

    Release app@0.20.1

--- Casks/tuist.rb
+++ Casks/tuist.rb
@@ -1,6 +1,6 @@
 cask "tuist" do
     version "app@0.20.1"
-    sha256 "9c4971cac672b2eb6b204343bf300459bb06875a9292f85ef4203a7dd38989bc"
+    sha256 "02bcfcd11e1b1443baa47c72e4e1b2b510d6af09545c673e9a46f56c381218cc"
 
     url "https://github.com/tuist/tuist/releases/download/app@app@0.20.1/Tuist.dmg"
     name "Tuist"
