8c0617e9444dac23f9ff841927e9e06a754bda8e
Pedro Piñera Buendía <663605+pepicrft@users.noreply.github.com>
2025-07-25 17:17:24 +0200

    Fix 4.55.6 sha256

--- Formula/tuist@4.55.6.rb
+++ Formula/tuist@4.55.6.rb
@@ -2,7 +2,7 @@ class TuistAT4556 < Formula
   desc "Create, maintain, and interact with Xcode projects at scale"
   homepage "https://tuist.io"
   url "https://github.com/tuist/tuist/releases/download/4.55.6/tuist.zip"
-  sha256 "10400d94f1eed5df5b1232c5ea0bf4a801914f82844d5fb59e90f66243a92c1f"
+  sha256 "c37e89f247582a0dc6669f17c4cebc004badb4465db4b21e201f71cc08a21b52"
   license "MIT"
   head "https://github.com/tuist/tuist.git", branch: "main"
 
