203f45ffb206b7e121db0a7a32674d8984296c43
fortmarek <marekfort@me.com>
2025-09-03 11:24:01 +0200

    Remove app prefix from version

--- Casks/tuist.rb
+++ Casks/tuist.rb
@@ -1,5 +1,5 @@
 cask "tuist" do
-    version "app@0.20.2"
+    version "0.20.2"
     sha256 "cb9c8a7291859e3b39e8f68792ebc7e800b4dda090ab617b4a7f22a822042ada"
 
     url "https://github.com/tuist/tuist/releases/download/app@0.20.2/Tuist.dmg"
