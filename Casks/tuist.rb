cf08cca10ec3179c798540f5462b1a5e6a3dd26c
fortmarek <marekfort@me.com>
2025-09-03 11:22:32 +0200

    Fix tuist.rb

--- Casks/tuist.rb
+++ Casks/tuist.rb
@@ -2,7 +2,7 @@ cask "tuist" do
     version "app@0.20.2"
     sha256 "cb9c8a7291859e3b39e8f68792ebc7e800b4dda090ab617b4a7f22a822042ada"
 
-    url "https://github.com/tuist/tuist/releases/download/app@app@0.20.2/Tuist.dmg"
+    url "https://github.com/tuist/tuist/releases/download/app@0.20.2/Tuist.dmg"
     name "Tuist"
     desc "Tuist macOS app"
     homepage "https://github.com/tuist/tuist"
