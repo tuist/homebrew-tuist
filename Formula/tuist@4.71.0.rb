308a04c227f39638fa085c9a79b6106f83a532f9
fortmarek <9371695+fortmarek@users.noreply.github.com>
2025-09-12 09:53:46 +0000

    Release 4.71.0

---
+++ Formula/tuist@4.71.0.rb
@@ -0,0 +1,21 @@
+class TuistAT4710 < Formula
+  desc "Create, maintain, and interact with Xcode projects at scale"
+  homepage "https://tuist.io"
+  url "https://github.com/tuist/tuist/releases/download/4.71.0/tuist.zip"
+  sha256 "9694aa991d8d74f67c77372cb3bc610babdf0b46097a867133241099ebbdd377"
+  license "MIT"
+  head "https://github.com/tuist/tuist.git", branch: "main"
+
+  depends_on macos: :monterey
+
+  def install
+    bin.install "tuist"
+    lib.install "ProjectDescription.framework"
+    lib.install "ProjectDescription.framework.dSYM"
+    share.install "Templates"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("${bin}/tuist version")
+  end
+end
